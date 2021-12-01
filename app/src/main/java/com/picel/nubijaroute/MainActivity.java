28c2c9ad583cfbe3d7e865c29af15818d601bd44
picel <picel@picel.net>
2021-12-02 01:14:44 +0900

    [Add] 2nd Activity

--- app/src/main/java/com/picel/nubijaroute/MainActivity.java
+++ app/src/main/java/com/picel/nubijaroute/MainActivity.java
@@ -1,19 +1,38 @@
 package com.picel.nubijaroute;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import net.daum.mf.map.api.MapView;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnOpenSearch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab_search);
+        fab.setOnClickListener((v) -> {
+            Intent intent = new Intent(MainActivity.this, Search.class);
+            startActivity(intent);
+        });
 
         MapView mapView = new MapView(this);
 
---
+++ app/src/main/java/com/picel/nubijaroute/Search.java
@@ -0,0 +1,16 @@
+package com.picel.nubijaroute;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Search extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        System.out.println("Hello");
+    }
+}
